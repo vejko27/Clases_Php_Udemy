f0b9a4133e10dc5b5cab1b2e8f2a6805bd2bbb77
vejko27 <vejko27@hotmail.com>
2026-06-19 15:51:42 -0500

    5

--- arreglos_asociativos.php
+++ arreglos_asociativos.php
@@ -1,11 +1,15 @@
 <?php
 //En este capítulo veremos arreglos Asociativos
 
-$alex = array('telefono','edad','apellido','mexico');
+$victor = array('telefono' =>'980683910','edad'=>'41','apellido'=>'Canova','País'=>'Perú');
 
+echo $victor['telefono'] . '<br />';
 
+echo $victor['edad']  . '<br />';
 
+echo $victor['apellido']  . '<br />';
 
+echo 'La edad de Victor es: ' .$victor['edad'];
 
 
 
---
+++ arreglos_multidimensionales.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arreglos multidimensionales de PHP</title>
+</head>
+<body>
+    <?php
+//Ahora veremos arreglos multidimensionales de amigos uno dentro del otro
+  $amigos = array(
+     array('Miguel', 38),
+     array('Cereza', 36),
+     array('Diego',37)
+     );
+
+  echo $amigos[0][1] . '<br/>';
+  
+
+
+?>
+</body>
+</html>
+
