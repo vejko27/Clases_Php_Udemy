a8bb3264405261671866aa8aefeaa520e9949c05
vejko27 <vejko27@hotmail.com>
2026-07-06 17:59:05 -0500

    Clases_de_Ciclos

--- ciclo_while.php
+++ ciclo_while.php
@@ -1,3 +1,20 @@
 <?php
 //Acá vamos a ver ciclo While
+
+//Nuestra estructura es 
+//Primero variable externa
+//Y luego
+
+// while(){
+//     //Instrucciones
+// }
+
+$x = 20;
+
+while($x >= 1){
+    echo $x .' <br/>';
+
+    $x--;
+}
+
 ?>
---
+++ recorriendo_arreglos_con_ciclo_for_while.php
@@ -0,0 +1,31 @@
+<?php
+//Ahora vamos a recorrer ciclos con arreglos
+//con ciclo for_While
+$meses = array('Enero',
+'Febrero',
+'Marzo',
+'Abril',
+'Mayo',
+'Junio',
+'Julio',
+'Agosto',
+'Setiembre',
+'Octubre',
+'Noviembre',
+'Diciembre'
+);
+
+//Usamos count que nos tomará todos los valores sin necesidad de llenarlos
+//Uno mismo.
+
+//echo count($meses);
+
+// for($i = 0; $i < count($meses); $i++){
+//         //instrucciones
+//         echo $meses[$i] . '<br/>';
+// }
+
+//Ahora lo vamos a correr con while
+
+$contador = 0;
+?>
