b9dc3731fd8c5a05cb4dad1df5717c3ebc741c5c
vejko27 <vejko27@hotmail.com>
2026-09-19 23:54:06 -0500

    Include y require

---
+++ PHP/vista.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vista sobre logica realizada</title>
+</head>
+<body>
+    <h1>El resultado es:<?php  echo 'La suma es: '. suma(10,20);  ?></h1>
+    
+</body>
+</html>
