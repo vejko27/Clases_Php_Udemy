5d81cac57065a77e55f7175d439c40642b448c69
vejko27 <vejko27@hotmail.com>
2026-09-20 21:30:44 -0500

    39

---
+++ Declaraciones_tipo_de_devolucion.php
@@ -0,0 +1,6 @@
+<?php
+//Declaraciones tipo devolución
+
+
+
+?>
--- declaraciones_tipo_escalar.php
+++ declaraciones_tipo_escalar.php
@@ -1,3 +1,13 @@
 <?php
 //Declaraciones de tipo escalar.
-?>
+
+declare(strict_types=1);
+function cuadrado($n1){
+    return $n1 * $n1;
+}
+    
+$n1 = '8';
+echo'El cuadrado del número ' . $n1 . ' es: ' . cuadrado($n1);
+
+
+//.
